b7a4ac01ab10fc4c10096503bfdfa3fe89b13ff8
zzl <zzl>
2025-08-23 23:53:48 +0800

    学到4.1

---
+++ Src/Client/Assets/MyScripts/Login.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Login : MonoBehaviour {
+
+	// Use this for initialization
+	public void Start () {
+		Network.NetClient.Instance.Init("127.0.0.1", 8000);
+        Network.NetClient.Instance.Connect();
+        SkillBridge.Message.NetMessage msg = new SkillBridge.Message.NetMessage();
+        msg.Request = new SkillBridge.Message.NetMessageRequest();
+        msg.Request.firstTestRequest = new SkillBridge.Message.FirstTestRequest();
+        msg.Request.firstTestRequest.Helloworld = "Hello World";
+        Network.NetClient.Instance.SendMessage(msg);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
