92a70c282a3f636cfc256bf0b6fae6a2239ca951
1418498464@qq.com <1418498464@qq.com>
2025-09-05 18:53:09 +0800

    9.5 准备改成navicat

---
+++ Src/Client/Assets/Scripts/UI/UIWindow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class UIWindow : MonoBehaviour {
+
+    public delegate void CloseHandler(UIWindow sender, WindowResult result);
+    public event CloseHandler OnClose;
+
+    public virtual System.Type Type { get { return this.GetType(); } }
+
+    public enum WindowResult
+    {
+        None=0,
+        Yes,
+        No
+    }
+	
+    public void Close(WindowResult result = WindowResult.None)
+    {
+        UIManager.Instance.Close(Type);
+        if (OnClose != null)
+        {
+            OnClose(this, result);
+        }
+        OnClose = null;
+    }
+    
+    public virtual void OnCloseClick()
+    {
+        Close();
+    }
+
+    public virtual void OnYesClick()
+    {
+        Close(WindowResult.Yes);
+    }
+}
